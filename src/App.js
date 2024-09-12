2744a3b0fbd959f769ab43f2e03a549568d13fbe
zaman <zaman342003@gmail.com>
2024-09-12 13:03:45 -0700

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import ApplicationLogin from "./Components/ApplicationLogin";
 import Login from "./Components/Login";
 import SideNavbar from "./Components/SideNav";
-import PageNotFound from "./Components/common/pageNotFound";
 
+import Dashboard from "./Components/dashboard";
 function App() {
   return (
     <div>
       <Router>
         {" "}
-        <SideNavbar />
+        {/* <SideNavbar /> */}
+        <Dashboard />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/applicationLogin" element={<ApplicationLogin />} />
---
+++ src/Components/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SideNavbar from "./SideNav";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Inside dashboard file</h2>
+      <SideNavbar />
+    </div>
+  );
+};
+
+export default Dashboard;
