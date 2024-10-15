f73424436e028f8cd1c1de78d6b0b68e459574bc
Muhammadbilalhaider <haiderb894@gmail.com>
2024-10-16 03:38:41 +0500

    working on sign in google

--- client/src/Components/dashboard.jsx
+++ client/src/Components/dashboard.jsx
@@ -6,25 +6,26 @@ import Navbar from './Navbar';
 
 const Dashboard = () => {
   const navigate = useNavigate();
-
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const token = params.get('token');
 
     if (token) {
       localStorage.setItem("authToken", token);
-      navigate("/");
+      
+      navigate("/"); 
     } else {
       const storedToken = localStorage.getItem("authToken");
-      if (!storedToken) {
-        navigate("/login");
-      }else
+      const currentPath = window.location.pathname;
+      if (!storedToken && !currentPath.startsWith('/forgot-password') && !currentPath.startsWith('/resetpassword')) {
+        navigate('/login');
+      }else if(storedToken && currentPath.startsWith('/login'))
       {
-        navigate("/")
+        navigate('/')
       }
     }
   }, [navigate]);
-
+  
   return (
     <div>
       {localStorage.getItem("authToken") ? <Navbar /> : null}
